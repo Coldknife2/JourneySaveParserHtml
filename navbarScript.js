7cecfcd4a57c972834e1979ba489c3c0c8336c64
arckoor <33837362+arckoor@users.noreply.github.com>
2022-02-01 19:02:24 +0100

    feature(navbar): added navigation between html files

---
+++ navbarScript.js
@@ -0,0 +1,16 @@
+let navbar = document.getElementById("navbarContainer");
+let dropSymbol = document.getElementById("dropSymbol");
+let expanded = true;
+
+function toggleNavbar() {
+    if (expanded) {
+        navbar.classList.remove("navbarOffsetRetracted");
+        navbar.classList.add("navbarOffsetExpanded");
+        dropSymbol.style.transform = "rotate(90deg)";
+    } else {
+        navbar.classList.add("navbarOffsetRetracted");
+        navbar.classList.remove("navbarOffsetExpanded");
+        dropSymbol.style.transform = "rotate(270deg)";
+    }
+    expanded = !expanded;
+}
