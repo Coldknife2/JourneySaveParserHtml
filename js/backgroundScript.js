ef577a634b17cc60941e666b5a2aa955bff611e8
Remi Boussu <remi.sylux@gmail.com>
2022-02-23 22:37:31 +0100

    feat(sunny): added style difference for when it's sunny on the background

--- js/backgroundScript.js
+++ js/backgroundScript.js
@@ -6,4 +6,15 @@ function trig(x) {
     return -0.00694444*x**2+0.1666666*x;
 }
 
-document.body.style.backgroundImage = `url(./images/cmatw_${Math.random() > determineTimePercentage() ? "sunny" : "dark"}.png)`;
+
+const randomPick = Math.random() > determineTimePercentage();
+document.body.style.backgroundImage = `url(./images/cmatw_${ randomPick ? "sunny" : "dark"}.png)`;
+
+if (randomPick)
+{
+    const link = document.createElement("link");
+    link.href = "./css/sunnyStyle.css";
+    link.rel = "stylesheet";
+    link.type = "text/css";
+    document.getElementsByTagName("head")[0].appendChild(link);
+}
