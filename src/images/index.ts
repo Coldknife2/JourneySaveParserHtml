c8e262a2bdd497fbe261e60e2805c6be91c2820c
arckoor <33837362+arckoor@users.noreply.github.com>
2022-06-16 17:41:27 +0200

    feature(location): images to show in-game location

--- src/images/index.ts
+++ src/images/index.ts
@@ -49,7 +49,70 @@ import level9 from "./levels/9.png";
 import level10 from "./levels/10.png";
 import level11 from "./levels/11.png";
 
+import CSS0 from "./locations/CS/S0.png";
+import CSS1 from "./locations/CS/S1.png";
+import CSS2 from "./locations/CS/S2.png";
+import CSM0 from "./locations/CS/M0.png";
+
+import BBS0 from "./locations/BB/S0.png";
+import BBS1 from "./locations/BB/S1.png";
+import BBS2 from "./locations/BB/S2.png";
+import BBM0 from "./locations/BB/M0.png";
+
+import PDS0 from "./locations/PD/S0.png";
+import PDS1 from "./locations/PD/S1.png";
+import PDS2 from "./locations/PD/S2.png";
+import PDS3 from "./locations/PD/S3.png";
+import PDM0 from "./locations/PD/M0.png";
+import PDM1 from "./locations/PD/M1.png";
+
+import SCS0 from "./locations/SC/S0.png";
+import SCS1 from "./locations/SC/S1.png";
+import SCS2 from "./locations/SC/S2.png";
+import SCM0 from "./locations/SC/M0.png";
+import SCM1 from "./locations/SC/M1.png";
+
+import UGS0 from "./locations/UG/S0.png";
+import UGS1 from "./locations/UG/S1.png";
+import UGS2 from "./locations/UG/S2.png";
+import UGS3 from "./locations/UG/S3.png";
+import UGM0 from "./locations/UG/M0.png";
+
+import TOS0 from "./locations/TO/S0.png";
+import TOS1 from "./locations/TO/S1.png";
+import TOS2 from "./locations/TO/S2.png";
+import TOS3 from "./locations/TO/S3.png";
+import TOM0 from "./locations/TO/M0.png";
+
+import SNM0 from "./locations/SN/M0.png";
+import SNM1 from "./locations/SN/M1.png";
+
 export const sym = [sym0, sym1, sym2, sym3, sym4, sym5, sym6, sym7, sym8, sym9, sym10, sym11, sym12, sym13, sym14, sym15, sym16, sym17, sym18, sym19, sym20];
-export const arrow = [[arrow1L, arrow2L, arrow3L, arrow4L], [arrow1R, arrow2R, arrow3R, arrow4R]];
-export const robe = [[red1, red2, red3, red4], [white2, white3, white4]];
 export const level = [level1, level2, level3, level4, level5, level6, level7, level8, level9, level10, level11];
+export const arrow = [
+	[arrow1L, arrow2L, arrow3L, arrow4L],
+	[arrow1R, arrow2R, arrow3R, arrow4R]
+];
+export const robe = [
+	[red1, red2, red3, red4],
+	[white2, white3, white4]
+];
+export const locations = [
+	[
+		[CSS0, CSS1, CSS2],
+		[BBS0, BBS1, BBS2],
+		[PDS0, PDS1, PDS2, PDS3],
+		[SCS0, SCS1, SCS2],
+		[UGS0, UGS1, UGS2, UGS3],
+		[TOS0, TOS1, TOS2, TOS3]
+	],
+	[
+		[CSM0],
+		[BBM0],
+		[PDM0, PDM1],
+		[SCM0, SCM1],
+		[UGM0],
+		[TOM0],
+		[SNM0, SNM1]
+	]
+];
