7f89f80421299b19334f0bbf22ce472799b79599
arckoor <33837362+arckoor@users.noreply.github.com>
2022-07-01 22:13:36 +0200

    feature(webp): added script to convert to webp

---
+++ scripts/webpCompression.js
@@ -0,0 +1,138 @@
+const { exec } = require("child_process");
+const fs = require("fs");
+const fsWalk = require("walk");
+const minimist = require("minimist");
+const path = require("path");
+
+function parseBool(str) {
+	if (typeof str === "string") {
+		str = str.toLowerCase();
+		if (str === "true" || str === "false") {
+			return str === "true";
+		}
+	}
+	return Boolean(str);
+}
+
+function parsePath(key, name, def) {
+	if (key in argv) {
+		const temp = argv[key];
+		if (typeof temp !== "string") {
+			console.log(`\x1b[31m${name} must be a string.\x1b[0m`);
+			process.exit(9);
+		}
+		return temp;
+	}
+	return def;
+}
+
+function parsePresets(set) {
+	const presets = [
+		"cwebp -m 6 -q 100 -mt -v",
+		"cwebp -size 15000 -pass 10 -mt -v",
+		"cwebp -size 50000 -pass 10 -mt -v "
+	];
+	if ("s" in argv) {
+		set = argv["s"];
+		if (typeof set === "number") {
+			if (set < presets.length) {
+				return presets[set];
+			} else {
+				console.log(`\x1b[31mPreset ${set} is not configured, the highest preset is ${presets.length-1}.\x1b[0m`);
+				process.exit(9);
+			}
+		} else {
+			return "cwebp" + set;
+		}
+	} else {
+		return presets[0];
+	}
+}
+
+function checkArguments(p, s, w) {
+	if (p && s) {
+		console.log("\x1b[31mPath and file are mutually exclusive.\x1b[0m");
+		process.exit(9);
+	} else if (!p && !s) {
+		console.log("\x1b[31mEither a path or a file need to be specified.\x1b[0m");
+		process.exit(9);
+	} else if (s && w) {
+		console.log("\x1b[31mCannot walk a single file.\x1b[0m");
+		process.exit(9);
+	}
+}
+
+function makeWebP(inFile, outFile) {
+	exec(preset + ` -o ${outFile}.webp ${inFile}`, (err, _, stderr) => {
+		if (err) {
+			throw err;
+		}
+		if (stderr) {
+			console.log(stderr);
+		}
+		if (deleteInFile) {
+			fs.unlink(inFile, (err => {
+				if (err) {
+					throw err;
+				}
+			}));
+		}
+	});
+}
+
+function showHelp() {
+	console.log(`
+	-h HELP   | Shows this help.
+	-p PATH   | Converts all images in the top level folder of the path. Cannot be used with the -f option. (-p path/to/folder)
+	-f FILE   | Converts the single image at the specified path. Cannot be used with the -p option. (-f path/to/file.png)
+	-w WALK   | Use with -p, converts all image files in the folder and all subfolders. (-w 1)
+	-d DELETE | Deletes the input file after the conversion is complete. (-d 1)
+	-s PRESET | Uses a predefined preset via the submitted index or uses the custom preset with " -flags". The string must start with a space. (-s " -v -mt")
+	`);
+}
+
+const argv = minimist(process.argv.slice(2));
+
+if ("h" in argv) {
+	showHelp();
+	process.exit(0);
+}
+
+const walkDir = "w" in argv ? parseBool(argv["w"]) : false;
+const deleteInFile = "d" in argv ? parseBool(argv["d"]) : false;
+const pathToDir = parsePath("p", "Path (-p)", false);
+const singleFile = parsePath("f", "File (-f)", false);
+const preset = parsePresets();
+checkArguments(pathToDir, singleFile, walkDir);
+
+if (walkDir) {
+	fsWalk.walk(pathToDir).on("file", function(root, fileStats, next) {
+		const [f, ext] = fileStats.name.split(".");
+		if (ext === "png") {
+			makeWebP(path.join(root, fileStats.name), path.join(root, f));
+		}
+		next();
+	});
+
+} else if (singleFile) {
+	if (fs.lstatSync(singleFile).isFile()) {
+		const [f, ext] = path.basename(singleFile).split(".");
+		if (ext === "png") {
+			makeWebP(singleFile, path.join(path.dirname(singleFile), f));
+		}
+	}
+} else {
+	fs.readdir(pathToDir, (err, files) => {
+		if (err) {
+			throw err;
+		}
+		files.forEach(file => {
+			if (fs.lstatSync(pathToDir + file).isFile()) {
+				const [f, ext] = file.split(".");
+				if (ext === "png") {
+					makeWebP(path.join(pathToDir, file), path.join(pathToDir, f));
+				}
+			}
+		});
+	});
+}
