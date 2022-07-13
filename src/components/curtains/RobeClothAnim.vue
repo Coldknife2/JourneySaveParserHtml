9400e285dfec16f038386fb3828b796ba9c0d812
ArcKoor <33837362+ArcKoor@users.noreply.github.com>
2022-07-13 11:42:29 +0200

    refactor(robe): improved wind effect

--- src/components/curtains/RobeClothAnim.vue
+++ src/components/curtains/RobeClothAnim.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { randomFloat, lerp, limitDecimals, shuffle } from "@/ts/math";
+import { randomFloat } from "@/ts/math";
 import { robe } from "@/images";
 import { Plane } from "vue-curtains";
 import { Texture } from "curtainsjs";
@@ -39,6 +39,7 @@ export default defineComponent({
 			planeProps: {
 				vertexShader: vertexShader,
 				fragmentShader: fragmentShader,
+				visible: false,
 				widthSegments: 20,
 				heightSegments: 40,
 				uniforms: {
@@ -47,54 +48,27 @@ export default defineComponent({
 						type: "1f",
 						value: 0
 					},
-					movScaleLeft: {
-						name: "uMovScaleLeft",
+					seed: {
+						name: "uSeed",
 						type: "1f",
-						value: .5
-					},
-					movScaleRight: {
-						name: "uMovScaleRight",
-						type: "1f",
-						value: .5
+						value: randomFloat(5000, 40000)
 					}
 				},
 			},
 			activeTexture: Texture,
-			plane: Plane,
 			currentTextureIndex: 0,
-			ready: false,
-			movScale: {
-				left: {
-					default: .5,
-					target: .5
-				},
-				right: {
-					default: .5,
-					target: .5
-				},
-				transition: 0,
-				rise: false,
-				decay: false
-			}
+			ready: false
 		};
 	},
 	mounted() {
 		this.currentTextureIndex = this.robeData;
 	},
-	beforeUnmount() {
-		this.plane.remove();
-	},
 	methods: {
 		onRender(plane: typeof Plane) {
-			if (!this.ready) {
-				plane.visible = false;
-			}
 			this.textureSwap(plane);
-			this.handleTransition(plane);
 			plane.uniforms.time.value++;
 		},
 		onReady(plane: typeof Plane) {
-			this.plane = plane;
 			this.activeTexture = plane.createTexture({
 				sampler: "uTexture",
 				fromTexture: plane.textures[this.currentTextureIndex]
@@ -106,38 +80,6 @@ export default defineComponent({
 			if (this.ready && this.robeData !== this.currentTextureIndex) {
 				this.currentTextureIndex = this.robeData;
 				this.activeTexture.setSource(plane.images[this.currentTextureIndex]);
-			}
-		},
-		handleTransition(plane: typeof Plane) {
-			const movScale = this.movScale;
-			const leftTarget = this.movScale.left;
-			const rightTarget = this.movScale.right;
-			const leftActual = plane.uniforms.movScaleLeft;
-			const rightActual = plane.uniforms.movScaleRight;
-
-			if (!movScale.rise && !movScale.decay) {
-				const [upSide, downSide] = shuffle([leftTarget, rightTarget]);
-				upSide.target = limitDecimals(randomFloat(0.2, 0.5), 3);
-				downSide.target = 0.2;
-				movScale.rise = true;
-			} else if (movScale.rise) {
-				leftActual.value = limitDecimals(lerp(leftTarget.default, leftTarget.target, movScale.transition), 3);
-				rightActual.value = limitDecimals(lerp(rightTarget.default, rightTarget.target, movScale.transition), 3);
-				movScale.transition = limitDecimals(movScale.transition + 0.001, 5);
-				if (movScale.transition === 1) {
-					movScale.rise = false;
-					movScale.decay = true;
-					movScale.transition = 0;
-				}
-			} else {
-				leftActual.value = limitDecimals(lerp(leftTarget.target, leftTarget.default, movScale.transition), 3);
-				rightActual.value = limitDecimals(lerp(rightTarget.target, rightTarget.default, movScale.transition), 3);
-				movScale.transition = limitDecimals(movScale.transition + 0.005, 5);
-				if (movScale.transition === 1) {
-					movScale.rise = false;
-					movScale.decay = false;
-					movScale.transition = 0;
-				}
 			}
 		}
 	}
