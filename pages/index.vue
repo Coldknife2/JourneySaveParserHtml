38be53fe0dc8562a1e593b68646f2a3af83fdae9
arckoor <33837362+arckoor@users.noreply.github.com>
2023-02-12 00:30:31 +0100

    refactor: move index script to <script setup>

--- pages/index.vue
+++ pages/index.vue
@@ -1,48 +1,33 @@
-<template>
-	<ParserPlayerDisplay :companions="companions" :past-companions="pastCompanions">
-		<template #heading>
-			<ParserPlayerDisplayHeading player-type="Contributors" />
-		</template>
-		<template #pastHeading>
-			<ParserPlayerDisplayHeading player-type="test" use-past-cmatw />
-		</template>
-	</ParserPlayerDisplay>
-</template>
-
-<script lang="ts">
+<script setup lang="ts">
 import { Companions } from "utils/interfaces";
 
-export default defineComponent({
-	data() {
-		return {
-			clearData: true,
-			companions: new Array<Companions>(),
-			pastCompanions: new Array<Companions>(),
-			saves: useSaves()
-		};
-	},
-	mounted() {
-		const companionNumber = readData(this.saves, "u8", offsets.companionAmount) as number;
-		for (let i = 0; i < 8; i++) {
-			const tempNameBuffer = readData(this.saves, "u8", offsets.companionName+offsets.companionOffset*i, 24) as Uint8Array;
-			const nameBuffer = tempNameBuffer.subarray(0, tempNameBuffer.indexOf(0x00));
-			const name = new TextDecoder().decode(nameBuffer);
+const saves = useSaves().value;
+let companions = new Array<Companions>();
+let pastCompanions = new Array<Companions>();
 
-			const steamIdV3int32 = readData(this.saves, "u32", offsets.companionID+offsets.companionOffset*i) as number;
-			const steamIdV3 = `[U:1:${steamIdV3int32}]`;
-			const safeHtmlSteamIdV3 = encodeURIComponent(steamIdV3);
-			const steamLink = "https://steamcommunity.com/profiles/" + safeHtmlSteamIdV3;
+const companionNumber = readData(saves, "u8", offsets.companionAmount);
+for (let i = 0; i < 8; i++) {
+	const tempNameBuffer = readData(saves, "u8", offsets.companionName+offsets.companionOffset*i, 24) as Uint8Array;
+	const nameBuffer = tempNameBuffer.subarray(0, tempNameBuffer.indexOf(0x00));
+	const name = new TextDecoder().decode(nameBuffer);
 
-			const symbolNumber = readData(this.saves, "u8", offsets.companionSymbol+offsets.companionSymbolOffset*i) as number;
-			const symbol = 0xF101 + symbolNumber;
+	const steamIdV3int32 = readData(saves, "u32", offsets.companionID+offsets.companionOffset*i) as number;
+	const steamIdV3 = `[U:1:${steamIdV3int32}]`;
+	const safeHtmlSteamIdV3 = encodeURIComponent(steamIdV3);
+	const steamLink = "https://steamcommunity.com/profiles/" + safeHtmlSteamIdV3;
 
-			const companionData = { l: steamLink, n: name, s: symbol };
-			if (steamIdV3int32 !== 0) {
-				(i < companionNumber) ? this.companions.push(companionData) : this.pastCompanions.push(companionData);
-			} else {
-				break;
-			}
-		}
+	const symbolNumber = readData(saves, "u8", offsets.companionSymbol+offsets.companionSymbolOffset*i) as number;
+	const symbol = 0xF101 + symbolNumber;
+
+	const companionData = { l: steamLink, n: name, s: symbol };
+	if (steamIdV3int32 !== 0) {
+		(i < companionNumber) ? companions.push(companionData) : pastCompanions.push(companionData);
+	} else {
+		break;
 	}
-});
+}
 </script>
+
+<template>
+	<ParserPlayerDisplay :companions="companions" :past-companions="pastCompanions" />
+</template>
